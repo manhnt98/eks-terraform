8f06f2bb6c24e5c2f5d5d3142350d8e3b83cfac3
manh.nguyentien@vndirect.com.vn <contact.tienmanh@gmail.com>
2023-04-26 10:27:36 +0700

    Update files

--- route-table-association.tf
+++ route-table-association.tf
@@ -3,6 +3,11 @@ resource "aws_route_table_association" "public1" {
   route_table_id = aws_route_table.public.id
 }
 
+resource "aws_route_table_association" "public2" {
+  subnet_id      = aws_subnet.public_2.id
+  route_table_id = aws_route_table.public.id
+}
+
 resource "aws_route_table_association" "private1" {
   subnet_id      = aws_subnet.private_1.id
   route_table_id = aws_route_table.private.id
